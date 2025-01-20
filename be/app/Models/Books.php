d6e0fc03f0f5f1a632129152edf4c17318758678
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-01-21 04:55:47 +0700

    clear code

--- be/app/Models/Books.php
+++ be/app/Models/Books.php
@@ -8,7 +8,6 @@ class Books extends Model
 {
     protected $guarded = [];
 
-    // Relasi dengan model Peminjaman
     public function peminjamans()
     {
         return $this->hasMany(Peminjaman::class, 'book_id');
